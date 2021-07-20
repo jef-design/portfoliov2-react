0fbf9e37dfed9efd6d2ed4031e908c2ec1293130
jef-design <bermejojff97@gmail.com>
2021-07-20 19:01:25 +0800

    fix skill icon

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -12,14 +12,22 @@ function About() {
                 <div className="about__wrapper">
                     <div className="about__info">
                         <p>
-                            &nbsp;&nbsp;&nbsp;&nbsp;Hi i'm <b>Jeff</b>, <b>Front-end Web Developer</b> i specialise in creating beautiful,
-                            usable, professional website using best practice.
-                            all my works are lovingly hand coded.
+                            &nbsp;&nbsp;&nbsp;&nbsp;Hi i'm <b>Jeff</b>,{" "}
+                            <b>Front-end Web Developer</b> i specialise in
+                            creating beautiful, usable, professional website
+                            using best practice. all my works are lovingly hand
+                            coded.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam corrupti rem possimus excepturi maiores quos fugit architecto totam perferendis, ex alias, aliquam ab, recusandae quia.
                         </p>
                     </div>
                     <div className="about__skill">
                         <div className="skill__container">
-                            <h3 className="skill__title">Here are a few technologies I've been working with recently:</h3>
+                            <h3 className="skill__title">
+                                Here are a few technologies I've been working
+                                with recently:
+                            </h3>
                             <h3>Languages</h3>
                             <div className="skill__wrapper">
                                 <div className="skill">
@@ -43,15 +51,15 @@ function About() {
                         <div className="skill__container">
                             <h3>Libraries/Frameworks</h3>
                             <div className="skill__wrapper ">
-                                <div className="skill">
+                                <div className="skill skill2">
                                     <i class="bx bxl-react"></i>
                                     <p>React.js</p>
                                 </div>
-                                <div className="skill">
+                                <div className="skill skill2">
                                     <i class="bx bxl-bootstrap"></i>
                                     <p>Bootstrap</p>
                                 </div>
-                                <div className="skill">
+                                <div className="skill skill2">
                                     <i class="bx bxl-redux"></i>
                                     <p>Redux</p>
                                 </div>
@@ -60,17 +68,17 @@ function About() {
                         <div className="skill__container">
                             <h3>Tools</h3>
                             <div className="skill__wrapper">
-                                <div className="skill">
-                                <i class="bx bxl-figma"></i>
-                                <p>Figma</p>
+                                <div className="skill skill3">
+                                    <i class="bx bxl-figma"></i>
+                                    <p>Figma</p>
                                 </div>
-                                <div className="skill">
-                                <i class="bx bxl-git"></i>
-                                <p>Git</p>
+                                <div className="skill skill3">
+                                    <i class="bx bxl-git"></i>
+                                    <p>Git</p>
                                 </div>
-                                <div className="skill">
-                                <i class="bx bxl-github"></i>
-                                <p>Github</p>
+                                <div className="skill skill3">
+                                    <i class="bx bxl-github"></i>
+                                    <p>Github</p>
                                 </div>
                             </div>
                         </div>
