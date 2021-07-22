a1da2d3f420acef4b07864d7d1039f11e1dea612
jef-design <bermejojff97@gmail.com>
2021-07-22 19:43:47 +0800

    burger mobile menu

--- src/components/main/Header/Header.js
+++ src/components/main/Header/Header.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from "react-router-dom";
 import {Helmet} from "react-helmet";
 
 const Header = () => {
+    const [isOpen,setIsOpen] = useState(false);
     return (
         <React.Fragment>
             <Helmet>
@@ -10,16 +11,16 @@ const Header = () => {
             </Helmet>
             <header className="header container">
                 <div className="header__wrapper">
-                <i className='bx bx-menu'></i>
+                <i className='bx bx-menu' onClick={()=> setIsOpen(!isOpen)}></i>
                     <div className="header__title">
-                        <Link to="/"><p>JEFF</p></Link>
+                        <Link to="/" onClick={()=> setIsOpen(!isOpen)}><p>JEFF</p></Link>
                     </div>
-                    <ul className="nav__menu">
+                    <ul className="nav__menu" style={{display: isOpen ? "flex" : ""}}>
                         <li>
-                            <Link to="/project">Projects</Link>
+                            <Link to="/project" onClick={()=> setIsOpen(!isOpen)}>Projects</Link>
                         </li>
                         <li>
-                            <Link to="/about">About & Skill</Link>
+                            <Link to="/about" onClick={()=> setIsOpen(!isOpen)}>About & Skill</Link>
                         </li>
                     </ul>
                 </div>
